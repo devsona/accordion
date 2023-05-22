28fc33c0986b6016a65994964be9acc08ce02f13
devsona <122227306+devsona@users.noreply.github.com>
2023-05-22 00:56:58 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,50 +1,100 @@
-const firstQuestion = document.getElementById('first-q')
-const secondQuestion = document.getElementById('second-q')
-const thirdQuestion = document.getElementById('third-q')
-const fourthQuestion = document.getElementById('fourth-q')
-const fifthQuestion = document.getElementById('fifth-q')
+let height, weight
 
-firstQuestion.addEventListener('click', function() {
-    toggleAnswer(firstQuestion, '#first-p', 'first-arrow', 'first-heading');
-  });
-
-  secondQuestion.addEventListener('click', function() {
-    toggleAnswer(secondQuestion, '#second-p', 'second-arrow','second-heading');
-  });  
-
-  thirdQuestion.addEventListener('click', function(){
-    toggleAnswer(thirdQuestion, '#third-p', 'third-arrow','third-heading')
-  })
-  
-  fourthQuestion.addEventListener('click', function() {
-    toggleAnswer(fourthQuestion, '#fourth-p', 'fourth-arrow','fourth-heading');
-  });
-
-  fifthQuestion.addEventListener('click', function() {
-    toggleAnswer(fifthQuestion, '#fifth-p', 'fifth-arrow','fifth-heading');
-  });
-
-  firstQuestion.onclick = toggleAnswer(firstQuestion, '#first-p', 'first-arrow', 'first-heading');
-  secondQuestion.onclick = toggleAnswer(firstQuestion, '#second-p', 'second-arrow', 'second-heading');
-  thirdQuestion.onclick = toggleAnswer(firstQuestion, '#third-p', 'third-arrow', 'third-heading');
-  fourthQuestion.onclick = toggleAnswer(firstQuestion, '#fourth-p', 'fourth-arrow', 'fourth-heading');
-  fifthQuestion.onclick = toggleAnswer(firstQuestion, '#fifth-p', 'fifth-arrow', 'fifth-heading');
-
-
-
-function toggleAnswer(question, answer, arrow,heading) {
-    let answerEl = document.querySelector(answer)
-    let headingEl = document.getElementById(heading)
-    console.log(answerEl)
-    if (answerEl.style.display === 'none') {
-        answerEl.style.display = 'block'
-        document.getElementById(arrow).style.transform = 'rotate(180deg)';
-        question.removeEventListener('click', toggleAnswer);
-        headingEl.classList.add('toBold')
+function changeUnit() {
+  let unit = document.getElementById('radio-container');
+  unit.addEventListener('click', function(e) {
+    if (e.target.id == 'metric') {
+      document.getElementById('wxh-container').innerHTML = `        
+        <p>height</p>
+        <input type="number" class="wxh" placeholder="CM" id='cm' min="1" max="300"> 
+        <p>weight</p>
+        <input type="number" class="wxh" placeholder="KG" id="weight"  min="1" max="999">`;
     } else {
-      answerEl.style.display = 'none'
-      document.getElementById(arrow).style.transform = 'rotate(360deg)';
-      question.addEventListener('click', toggleAnswer);
-      headingEl.classList.remove('toBold')
+      document.getElementById('wxh-container').innerHTML = `             
+        <p>height</p>
+        <div>
+        <input type="number" class="wxh" placeholder="ft" id='feet'>
+        <input type="number" class="wxh" placeholder="in" id='inch'>
+        </div>
+        <p>weight</p>
+        <div>
+        <input type="number" class="wxh" placeholder="st" id='st'>
+        <input type="number" class="wxh" placeholder="lbs" id='pound'>
+        </div>`;
     }
+  });
+}
+
+function startCheck() {
+  document.getElementById('metric').checked = true;
+}
+
+startCheck();
+changeUnit();
+
+function fixedDecimal(num) {
+  return Number.parseFloat(num).toFixed(2);
+}
+
+function getBmi() {
+  if (document.getElementById('metric').checked) {
+    weight = document.getElementById('weight').value;
+    height = document.getElementById('cm').value;
+    let bmi = weight / Math.pow(height, 2);
+    bmi += bmi * 10000;
+    bmi = fixedDecimal(bmi);
+    return bmi;
+  } else {
+    let feet = document.getElementById('feet').value;
+    let inch = document.getElementById('inch').value;
+    let st = document.getElementById('st').value;
+    let pound = document.getElementById('pound').value;
+    
+    let heightInInches = (parseInt(feet) * 12) + parseInt(inch);
+    let weightInPounds = (parseInt(st) * 14) + parseInt(pound);
+    
+    let bmi = weightInPounds / Math.pow(heightInInches, 2) * 703;
+    bmi = fixedDecimal(bmi);
+    return bmi;
   }
+}
+
+function ifHealthy(){
+    let bmi = getBmi()
+    let healthyWord;
+    if(bmi < 18.5){
+        healthyWord = 'Underweight'
+    } else if (bmi <= 24.5){
+        healthyWord = 'Healthy'
+    } else if (bmi <= 30) {
+        healthyWord = 'Overweight'
+    } else if (bmi > 30){
+        healthyWord = 'Obese'
+    }else {
+        healthyWord = 'error please try again!'
+    }
+    return healthyWord   
+}
+
+
+
+
+document.addEventListener('input', function() {
+    let numberHtml = document.getElementById('bmi-answer');
+    let bmi = getBmi();
+    
+    if (isNaN(bmi) || !isFinite(bmi)) {
+      numberHtml.innerHTML = '0.00';
+      document.getElementById('bmi-answer-p-ending').innerHTML =`Add your weight and height details to recive more information about your bmi. Or scroll down to learn more!`
+    } else {
+      numberHtml.innerHTML = bmi;
+      document.getElementById('bmi-answer-p-ending').innerHTML =`Your BMI suggests you’re a <span>${ifHealthy()}</span> weight. `
+    }
+    if (bmi > 100 || bmi < 0){
+        numberHtml.innerHTML = 'Impomsible BMI'
+        numberHtml.style.fontSize = '2rem'
+    }
+
+});
+
+
